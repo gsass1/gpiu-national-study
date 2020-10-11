6de637d747af448c1fa67e236e3747fb3eb1a0f3
Gian Saß <gian.sass@mni.thm.de>
2020-10-11 20:17:40 +0200

    Refactor date code into CalendarUtil

--- app/controllers/study_calendar_controller.rb
+++ app/controllers/study_calendar_controller.rb
@@ -4,29 +4,10 @@ class StudyCalendarController < ApplicationController
 
   def index
     @study_iterations = current_user.country.study_iterations.accepted
-    @months = [] 
 
     if @study_iterations.any?
       @study_ranges = StudyRange.where(study_iteration_id: @study_iterations.pluck(:id))
-
-      @first_date = @study_ranges.first.start
-      @last_date = @study_ranges.last.end
-
-      current_month = @first_date.month
-      year = @first_date.year
-
-      total_months = ((@last_date - @first_date).to_i / 30.0).ceil
-
-      total_months.times do
-        if current_month > 12
-          current_month = 1
-          year = year + 1
-        end
-
-        @months.append year: year, month: current_month
-
-        current_month = current_month + 1
-      end
+      @months = CalendarUtil::collect_months(@study_ranges.first.start, @study_ranges.last.end)
     end
   end
 end
