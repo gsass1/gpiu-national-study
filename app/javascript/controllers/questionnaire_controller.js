cfcba11aee053c174536304c00899a74ff7f2dec
Gian Saß <gian.sass@mni.thm.de>
2020-10-14 16:40:10 +0200

    Use generic questionnaire stimulus controller for both forms

--- app/javascript/controllers/questionnaire_controller.js
+++ app/javascript/controllers/questionnaire_controller.js
@@ -1,12 +1,33 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  connect() {
+    document.querySelectorAll('[data-condition').forEach((elem) => {
+      this.onTriggerChange({ target: elem  });
+    }, this);
+  }
+
   onTriggerChange(e) {
     const elem = document.getElementById(e.target.getAttribute('data-elem'));
-    if (e.target.value == e.target.getAttribute('data-condition')) {
-      elem.style.display = "list-item"
+    const cond = e.target.getAttribute('data-condition');
+
+    let conditionMatched = false;
+    if (e.target.type == "radio") {
+      conditionMatched = (e.target.value == cond) && e.target.checked;
+    } else if (e.target.type == "checkbox") {
+      conditionMatched = e.target.checked == (cond == "true");
     } else {
-      elem.style.display = "none"
+      conditionMatched = e.target.value == cond;
+    }
+
+    if (conditionMatched) {
+      if (elem instanceof HTMLLIElement) {
+        elem.style.display = "list-item";
+      } else {
+        elem.style.display = "block";
+      }
+    } else {
+      elem.style.display = "none";
     }
   }
 }
