f062f7be4bd6cc883818db99c1de8b384f384f48
Felix Münscher <Felix.Muenscher@mni.thm.de>
2020-10-27 13:21:31 +0100

    Removes wip migration

--- db/migrate/20201019171253_rename_biopsy_state_columns.rb
+++
@@ -1,6 +0,0 @@
-class RenameBiopsyStateColumns < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :patients, :prostate_biopsy_state, :biopsy_state
-    rename_column :patients, :prostate_biopsy_outcome_state, :biopsy_outcome_state
-  end
-end
