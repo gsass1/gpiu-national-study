cec6ebfba436d8aa2784c2cf3de73a9b723c8c19
Gian Saß <gian.sass@mni.thm.de>
2020-11-05 13:03:10 +0100

    Can export biopsy data

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -28,11 +28,43 @@ class Patient < ApplicationRecord
   admin_custom_actions :admin_actions
 
   def admin_actions
-    [{
+    arr = [{
       name: "Open Include Form",
       color: :success,
-      route: [:edit_patient_identification_path, self.patient_identification.id],
+      route: [:edit_patient_identification_path, self.id, self.patient_identification.id],
     }]
+
+    if uti_ssi?
+      if uti_form_needed?
+        arr.push({
+          name: "Open UTI Form",
+          color: :info,
+          route: [:edit_patient_uti_questionnaire_path, self.id, self.uti_questionnaire.id],
+        })
+      end
+
+      if ssi_form_needed?
+        arr.push({
+          name: "Open SSI Form",
+          color: :info,
+          route: [:edit_patient_ssi_questionnaire_path, self.id, self.ssi_questionnaire.id],
+        })
+      end
+    elsif prostate_biopsy?
+      arr.push({
+        name: "Open Biopsy Form",
+        color: :info,
+        route: [:edit_patient_biopsy_questionnaire_path, self.id, self.biopsy_questionnaire.id],
+      })
+
+      arr.push({
+        name: "Open Biopsy Outcome Form",
+        color: :info,
+        route: [:edit_patient_biopsy_outcome_questionnaire_path, self.id, self.biopsy_outcome_questionnaire.id],
+      })
+    end
+
+    arr
   end
 
   validates :department_id, presence: true
