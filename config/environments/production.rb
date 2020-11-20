2c9ca1bb3e6a2a2f52e99d46dee98adf2622abf9
Gian Saß <gian.sass@mni.thm.de>
2020-11-20 15:58:01 +0100

    Add production email info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: 'national.esiu.org' }
+  config.action_mailer.default_options = { from: 'gpiu-serpens-mni@lists.thm.de' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'mailserv.fh-giessen.de',
+      port: 25,
+      domain: 'thm.de',
+      openssl_verify_mode: 'none'
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
