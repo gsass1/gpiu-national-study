c1a292e89cf11ddd0d0a8a9e29dbdc0a249fd819
Gian Saß <gian.sass@mni.thm.de>
2020-12-10 13:08:15 +0100

    Add some info to the regional admin dashboard so its not blank

--- app/controllers/regional_admin/dashboard_controller.rb
+++ app/controllers/regional_admin/dashboard_controller.rb
@@ -5,5 +5,10 @@ class RegionalAdmin::DashboardController < ApplicationController
 
   def index
     add_breadcrumb "Admin", regional_admin_country_dashboard_index_path(current_country.iso_2)
+
+    @user_count = @country.users.count
+    @hospitals_count = @country.hospitals.count
+    @departments_count = @country.departments.count
+    @study_iterations_count = @country.study_iterations.count
   end
 end
