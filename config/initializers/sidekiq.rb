ce68cb9b59bd170beccca9fea67c7b3a069bad04
Gian Saß <gian.sass@mni.thm.de>
2021-03-11 15:47:43 +0100

    Add redis url to env

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-redis_url = ENV['GPIU_NATIONAL_REDIS_URL'] || 'localhost:6379'
+redis_url = ENV['GPIU_NAT_REDIS_URL'] || 'localhost:6379'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{redis_url}/0", namespace: "gpiu_national_sidekiq_#{Rails.env}" }
