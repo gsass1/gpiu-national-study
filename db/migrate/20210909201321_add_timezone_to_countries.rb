7dbb1f22ac41fe45e948e115c01fb407100048b4
Gian Saß <gian.sass@mni.thm.de>
2021-09-09 23:14:09 +0200

    Already set default timezone in migration

--- db/migrate/20210909201321_add_timezone_to_countries.rb
+++ db/migrate/20210909201321_add_timezone_to_countries.rb
@@ -1,5 +1,5 @@
 class AddTimezoneToCountries < ActiveRecord::Migration[6.0]
   def change
-    add_column :countries, :timezone, :string
+    add_column :countries, :timezone, :string, default: "UTC"
   end
 end
