c715c67cad326ba3e3a27bdb50fd3adc0983b51b
Gian Saß <gian.sass@mni.thm.de>
2021-10-10 16:52:15 +0200

    Fix some validation bugs including booleans

--- app/models/ssi_questionnaire.rb
+++ app/models/ssi_questionnaire.rb
@@ -1,4 +1,6 @@
 class SsiQuestionnaire < UtiSsiQuestionnaire
+  include ValidateBoolean
+
   custom_questionnaire_state :ssi_state
 
   default_scope { where(form_type: :ssi) }
@@ -7,13 +9,14 @@ class SsiQuestionnaire < UtiSsiQuestionnaire
     self.form_type = :ssi
   end
 
+  validates_boolean :previous
+  validates_boolean :tractobstruction
+  validates :tractobjstructionspecify, presence: true, if: Proc.new { |f| f.tractobstruction }
 
-  validates :previous, presence: true
-  validates :tractobstruction, presence: true
-  validates :treatment, presence: true
+  validates_boolean :treatment
   validates :treatmentspecify, presence: true, if: Proc.new { |f| f.treatment }
 
-  validates :hospitalisation, presence: true
+  validates_boolean :hospitalisation
   validates :catheterduration, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   validates :intervention_contamination, presence: true
@@ -21,7 +24,7 @@ class SsiQuestionnaire < UtiSsiQuestionnaire
   validates :clinicaldiagnosis, presence: true
   validates :clinicaldiagnosisspecify, presence: true, if: Proc.new { |f| f.clinicaldiagnosis == 'other' }
 
-  validates :provennauti, presence: true
+  validates_boolean :provennauti
   validates :antibioticculture, presence: true
   validates :antimicrobial_treatment, presence: true
 end
--- app/models/uti_questionnaire.rb
+++ app/models/uti_questionnaire.rb
@@ -1,4 +1,6 @@
 class UtiQuestionnaire < UtiSsiQuestionnaire
+  include ValidateBoolean
+
   custom_questionnaire_state :uti_state
 
   default_scope { where(form_type: :uti) }
@@ -7,12 +9,15 @@ class UtiQuestionnaire < UtiSsiQuestionnaire
     self.form_type = :uti
   end
 
-  validates :previous, presence: true
-  validates :tractobstruction, presence: true
-  validates :treatment, presence: true
+  validates_boolean :previous
+
+  validates_boolean :tractobstruction
+  validates :tractobjstructionspecify, presence: true, if: Proc.new { |f| f.tractobstruction }
+
+  validates_boolean :treatment
   validates :treatmentspecify, presence: true, if: Proc.new { |f| f.treatment }
 
-  validates :hospitalisation, presence: true
+  validates_boolean :hospitalisation
   validates :catheterduration, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   validates :intervention_contamination, presence: true
@@ -20,7 +25,7 @@ class UtiQuestionnaire < UtiSsiQuestionnaire
   validates :clinicaldiagnosis, presence: true
   validates :clinicaldiagnosisspecify, presence: true, if: Proc.new { |f| f.clinicaldiagnosis == 'other' }
 
-  validates :provennauti, presence: true
+  validates_boolean :provennauti
   validates :antibioticculture, presence: true
   validates :antimicrobial_treatment, presence: true
 end
