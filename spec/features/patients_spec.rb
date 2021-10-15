153062e293927992ccdd1898e8a223ef6e364f52
Gian Saß <gian.sass@mni.thm.de>
2021-10-15 14:27:21 +0200

    Update patients test

--- spec/features/patients_spec.rb
+++ spec/features/patients_spec.rb
@@ -41,15 +41,59 @@ RSpec.describe 'Patient management' do
       expect(page).to have_content('123')
     end
 
-    # it 'should allow the user to lock and unlock patients', js: true do
-    #   visit patients_path
+    it 'should allow the user to lock and unlock patients', js: true do
+      visit patients_path
 
-    #   click_link 'New Patient', match: :first
-    #   fill_in 'Initial', with: '123'
-    #   click_button 'Create'
+      click_link 'New Patient', match: :first
+      fill_in 'Initial', with: '123'
+      click_button 'Create'
 
-    #   click_ujs_link find(:css, "a[title='Lock this patient (prevents further edits)'")
-    #   expect(Patient.last.locked?).to eq(true)
-    # end
+      find(:css, "a[title='Lock this patient (prevents further edits)'").click
+      expect(Patient.last.locked?).to eq(true)
+
+      find(:css, "a[title='Unlock this patient (enables further edits)'").click
+      expect(Patient.last.locked?).to eq(false)
+    end
+
+    it 'displays the questionnaire buttons for the appropriate infection types', js: true do
+      visit patients_path
+
+      # Create patient
+      click_link 'New Patient', match: :first
+      fill_in 'Initial', with: '123'
+      click_button 'Create'
+
+      # Given that I choose UTI
+      click_link 'Include Form ✕ (invalid/missing)'
+      select '1999', from: :patient_identification_birth_year
+      choose 'Male'
+      choose 'UTI'
+      click_button 'Update Patient Identification'
+
+      # I should see the UTI form and not the SSI form
+      visit patients_path
+      expect(page).to have_content('UTI Form ✕ (invalid/missing)')
+      expect(page).to_not have_content('SSI Form ✕ (invalid/missing)')
+
+      # Given that I choose SSI
+      click_link 'Include Form ✓ (valid)'
+      choose 'SSI'
+      click_button 'Update Patient Identification'
+
+      # I should see the SSI form and not the UTI form
+      visit patients_path
+      expect(page).to_not have_content('UTI Form ✕ (invalid/missing)')
+      expect(page).to have_content('SSI Form ✕ (invalid/missing)')
+
+      # Given that I choose both
+      click_link 'Include Form ✓ (valid)'
+      choose 'Both'
+      click_button 'Update Patient Identification'
+
+      # I should see the SSI form and the UTI form
+      visit patients_path
+      expect(page).to have_content('UTI Form ✕ (invalid/missing)')
+      expect(page).to have_content('SSI Form ✕ (invalid/missing)')
+    end
   end
 end
