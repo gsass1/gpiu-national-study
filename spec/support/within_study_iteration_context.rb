15b435a5efdca5fc67a7f47905d0dc512db49aa4
Gian Saß <gian.sass@mni.thm.de>
2021-10-18 13:29:01 +0200

    fix: 'within study iteration' context did properly create the employment

--- spec/support/within_study_iteration_context.rb
+++ spec/support/within_study_iteration_context.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.shared_context 'within study iteration', shared_context: :metadata do
   let(:hospital) { create(:hospital, country: user.country) }
   let(:department) { create(:department, hospital: hospital) }
-  let(:employment) { create(:employment, user: user, department: department) }
+  let!(:employment) { create(:employee, user: user, department: department) }
   let(:study_iteration) { create(:study_iteration, country: user.country, acceptance_state: 'accepted') }
+
 
   before do
     study_iteration.study_ranges.create!(start: 14.days.from_now, end: 30.days.from_now)
