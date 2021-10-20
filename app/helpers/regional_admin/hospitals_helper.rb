421d77ba2730230af79ede7b97b3ccb2df2ccb93
Gian Saß <gian.sass@mni.thm.de>
2021-10-20 13:13:53 +0200

    Remove empty helper modules

--- app/helpers/regional_admin/hospitals_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module RegionalAdmin
-  module HospitalsHelper
-  end
-end
--- app/helpers/regional_admin/users_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module RegionalAdmin
-  module UsersHelper
-  end
-end
