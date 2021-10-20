058e1517b7dfba38bc77889ff042285d41468d42
Gian Saß <gian.sass@mni.thm.de>
2021-10-20 13:15:04 +0200

    rubocop: run on app

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class NotificationsMailer < ApplicationMailer
   add_template_helper NotificationsHelper
 
@@ -6,11 +8,11 @@ class NotificationsMailer < ApplicationMailer
   def notification_email
     options_hash = params[:notification]
     @notification = OpenStruct.new({
-      actor: User.find(options_hash[:actor_id]),
-      recipient: User.find(options_hash[:recipient_id]),
-      action: options_hash[:action],
-      notifiable: options_hash[:notifiable_class].constantize.find(options_hash[:notifiable_id])
-    })
+                                     actor: User.find(options_hash[:actor_id]),
+                                     recipient: User.find(options_hash[:recipient_id]),
+                                     action: options_hash[:action],
+                                     notifiable: options_hash[:notifiable_class].constantize.find(options_hash[:notifiable_id])
+                                   })
     mail(to: @notification.recipient.email, subject: t("notifications.action.#{@notification.action}.title"))
   end
 end
