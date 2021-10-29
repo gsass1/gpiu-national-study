335a2d1dee06c667e531a58a2da8ad948a06621b
Gian Saß <gian.sass@mni.thm.de>
2021-10-29 13:06:44 +0200

    Clean up

--- app/helpers/admin/study_iterations_helper.rb
+++ app/helpers/admin/study_iterations_helper.rb
@@ -2,25 +2,6 @@
 
 module Admin
   module StudyIterationsHelper
-    def collapse_panel(title, id, table, &block)
-      content_tag :div, class: 'card mb-3' do
-        concat(
-          content_tag(:div, class: 'card-header bg-primary text-light') do
-            content_tag(:strong) do
-              link_to title, "#collapse#{id}", class: 'admin_collapse_link', role: :button, data: { toggle: 'collapse' },
-                                               aria: { expanded: false, controls: "collapse#{id}" }
-            end
-          end
-        )
-
-        concat(
-          content_tag(:div, id: "collapse#{id}", class: 'card-body collapse', style: (table ? 'padding:0;' : '')) do
-            yield block
-          end
-        )
-      end
-    end
-
     def admin_study_iteration_nav_link(tab)
       link_to admin_study_iteration_path(@study_iteration, tab: tab), class: "nav-link #{'active' if @tab == tab}" do
         tab.capitalize
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,52 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def bootstrap_class_for_flash(flash_type)
-    case flash_type
-    when 'success'
-      'alert-success'
-    when 'error'
-      'alert-danger'
-    when 'alert'
-      'alert-warning'
-    when 'notice'
-      'alert-info'
-    else
-      flash_type.to_s
-    end
-  end
-
-  def icon_class_for_flash(flash_type)
-    case flash_type
-    when 'success'
-      'check_circle'
-    when 'error'
-      'error'
-    when 'alert'
-      'warning'
-    when 'notice'
-      'announcement'
-    else
-      flash_type.to_s
-    end
-  end
-
   def is_landing_page?
     controller_name == 'site' && action_name == 'index'
   end
 
   def flag_image(code)
     flag_icon code.to_sym, class: 'national-flag', width: '200'
-  end
-
-  def admin_sidepanel_link(resource)
-    content_tag :li do
-      link_to "/admin/#{resource}" do
-        content_tag :strong do
-          controller_name == resource ? "> #{resource}" : resource
-        end
-      end
-    end
   end
 
   def unread_notifications_badge
