e8d721a8b4ee7c3409fdcc5c970dc90c00dde453
Gian Saß <gian.sass@mni.thm.de>
2021-11-23 11:32:23 +0100

    Adds more useful info to the regional dashboard

--- app/controllers/regional_admin/dashboard_controller.rb
+++ app/controllers/regional_admin/dashboard_controller.rb
@@ -9,10 +9,15 @@ module RegionalAdmin
     def index
       add_breadcrumb 'Admin', regional_admin_country_dashboard_index_path(current_country.iso_2)
 
+      @current_study_iteration = @country.current_study_iteration
+      @pending_study_iterations = @country.study_iterations.pending
+
       @user_count = @country.users.count
       @hospitals_count = @country.hospitals.count
       @departments_count = @country.departments.count
       @study_iterations_count = @country.study_iterations.count
+
+      @pending_hospitals = @country.hospitals.where(acceptance_state: :pending)
     end
   end
 end
